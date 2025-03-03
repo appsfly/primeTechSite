e89abafc298e71555c86ea4f711f0e51f10666cc
GidiSayDo <80324378+GidiSayDo@users.noreply.github.com>
2025-03-03 17:32:16 +0200

    built site for primetecg

--- js/home.script.js
+++ js/home.script.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const swiperGallery = new Swiper(".mySwiper", {
+    loop: true, // חזרה אינסופית
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000, // כל 3 שניות
+      disableOnInteraction: false,
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+  });
+
+  // קרוסלת המודעות - עם אנימציה חלקה יותר
   const newsSwiper = new Swiper(".newsSwiper", {
     loop: true,
     autoplay: {
@@ -16,5 +35,12 @@ document.addEventListener("DOMContentLoaded", function () {
       el: ".swiper-pagination",
       clickable: true,
     },
+  });
+
+  // הגדרת Lightbox2
+  lightbox.option({
+    resizeDuration: 200,
+    wrapAround: true,
+    fadeDuration: 300,
   });
 });
