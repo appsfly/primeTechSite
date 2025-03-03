a6bc271197ce5db6da249ef7c6e1624567d8c4b5
GidiSayDo <80324378+GidiSayDo@users.noreply.github.com>
2025-03-03 16:36:40 +0200

    added news board

--- js/home.script.js
+++ js/home.script.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".mySwiper", {
-    loop: true, // חזרה אינסופית
+  const newsSwiper = new Swiper(".newsSwiper", {
+    loop: true,
+    autoplay: {
+      delay: 1, // עיכוב מינימלי
+      disableOnInteraction: false,
+    },
+    speed: 5000, // זמן המעבר בין שקופיות (5 שניות)
+    slidesPerView: 1, // מציג שקופית אחת בכל פעם
+    spaceBetween: 20,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -9,18 +16,5 @@ document.addEventListener("DOMContentLoaded", function () {
       el: ".swiper-pagination",
       clickable: true,
     },
-    autoplay: {
-      delay: 3000, // כל 3 שניות
-      disableOnInteraction: false,
-    },
-    slidesPerView: 1, // מציג שקופית אחת בכל פעם
-    spaceBetween: 10,
-  });
-
-  // הגדרת Lightbox2
-  lightbox.option({
-    resizeDuration: 200,
-    wrapAround: true,
-    fadeDuration: 300,
   });
 });
